485fb9ab09934933ce851c6ccbb6d26146d682e2
ericgaona <ericgaona0413@gmail.com>
2020-06-07 10:33:38 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_uvxVwegGmV52rXybCJiriBz2009duXyALW',     #    este linea y la de abajo son para el stripe
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
